290b74592990bb7039e3711edde9e5ae09ab39b9
Lucas Dale <ld17852@bristol.ac.uk>
2020-02-15 13:37:15 +0000

    new_story basic form complete

--- graphite/public/js/new_story.js
+++ graphite/public/js/new_story.js
@@ -1,32 +1,59 @@
 let chapters = 1;
+let chapterCount = 1;
+let del = 0;
+let collapse = true;
 
 $("#accordionForm").on("click", ".addchapter", function() {
     chapters++;
-    $("#accordionForm").append(`<div class="card">
-    <div class="card-header" id="heading${chapters}">
-      <h2 class="mb-0">
-        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${chapters}" aria-expanded="false" aria-controls="collapse${chapters}">
-          Chapter ${chapters}
-        </button>
-      </h2>
-    </div>
-    <div id="collapse${chapters}" class="collapse" aria-labelledby="heading${chapters}" data-parent="#accordionForm">
-      <div class="card-body">
-        <div class="form-group">
-            <label for="name${chapters}">Chapter Name:</label>
-            <input class="form-control" id="name${chapters}" type="text" name="name${chapters}" placeholder="Chapter ${chapters}" />
+    chapterCount++;
+    $("#accordionForm").append(`
+    <div class="card">
+      <div class="card-header" id="heading${chapters}">
+        <div class="d-inline-flex justify-content-start">
+          <button class="delchapter btn btn-outline-danger" id="del${chapters}" type="button">-</button>
+          </div>
+        <div class="d-inline-flex justify-content-center">
+          <input class="form-control" id="name${chapters}" type="text" name="name${chapters}" placeholder="Chapter ${chapters}" 
+            value="Chapter ${chapters}" style="width:90%;" data-toggle="collapse" 
+            data-target="#collapse${chapters}" aria-expanded="false" aria-controls="collapse${chapters}" />
         </div>
-        <div class="form-group">
-            <label for="size${chapters}">Wordcount/Runtime:</label>
-            <input class="form-control" id="size${chapters}" type="number" name="size${chapters}" placeholder="2500" />
+        <div class="d-inline-flex justify-content-end">
+          <button class="addchapter btn btn-outline-primary" id="add${chapters}" type="button">+</button>
         </div>
-        <div class="form-group">
-            <label for="characters${chapters}">Characters:</label>
-            <textarea class="form-control" id="characters${chapters}" rows="3"></textarea>
-        </div>
-        <button class="delchapter btn btn-outline-danger" id="del${chapters}" type="button" style="margin-right:0.5em">-</button>
-        <button class="addchapter btn btn-outline-primary" id="add${chapters}" type="button">+</button>
       </div>
-    </div>
+    <div class="collapse show" id="collapse${chapters}" aria-labelledby="heading${chapters}">
+      <div class="card-body">
+          <div class="form-group"><label for="size${chapters}">Wordcount/Runtime:</label><input class="form-control" id="size${chapters}" type="number" name="size${chapters}" placeholder="2500" /></div>
+          <div class="form-group"><label for="characters${chapters}">Characters:</label><textarea class="form-control" id="characters${chapters}" rows="3"></textarea></div>
+      </div>
     </div>`);
+});
+
+$("#accordionForm").on("click", ".delchapter", function(event) {
+  if(chapterCount > 1) {
+    $('#delconfirm').modal('show');
+    let id = event.target.id;
+    let match = /del([0-9]+)/.exec(id);
+    del = match[1];
+  } else {
+    $("#minchapter").modal();
+  }
+});
+
+$("#delyes").click(function() {
+  $("#del".concat(del)).closest(".card").remove();
+  chapterCount--;
+  //chapters--;
+});
+
+$("#collapseall").click(function(event) {
+  if(collapse) {
+    $(".card > .show").collapse('hide');
+    $("#collapseall").html("Expand All");
+    collapse = false;
+  } else {
+    $(".card > .collapse").collapse('show');
+    $("#collapseall").html("Collapse All");
+    collapse = true;
+  }
 });
--- graphite/routes/index.js
+++ graphite/routes/index.js
@@ -6,29 +6,34 @@ router.get('/', (req, res) => {
     res.render('index', { title: 'Graphite', home: 'active'});
   });
 
-  router.get('/login', (req, res) => {
-    res.render('login', { title: 'Graphite', login: 'active'});
-  });
+router.get('/login', (req, res) => {
+  res.render('login', { title: 'Graphite', login: 'active'});
+});
 
-  router.post('/login', (req, res) => {
-    console.log(req.body);
-    res.render('login', { title: 'Graphite', login: 'active'});
-  });
+router.post('/login', (req, res) => {
+  console.log(req.body);
+  res.render('login', { title: 'Graphite', login: 'active'});
+});
 
-  router.get('/account', (req, res) => {
-    res.render('account', { title: 'Graphite', account: 'active'});
-  });
+router.get('/account', (req, res) => {
+  res.render('account', { title: 'Graphite', account: 'active'});
+});
 
-  router.get('/stories', (req, res) => {
-    res.render('stories', { title: 'Graphite', stories: 'active'});
-  });
+router.get('/stories', (req, res) => {
+  res.render('stories', { title: 'Graphite', stories: 'active'});
+});
 
-  router.get('/new_story', (req, res) => {
-    res.render('new_story', { title: 'Graphite', stories: 'active'});
-  });
+router.get('/new_story', (req, res) => {
+  res.render('new_story', { title: 'Graphite', stories: 'active'});
+});
 
-  router.get('/graphs', (req, res) => {
-    res.render('graphs', { title: 'Graphite', graphs: 'active'});
-  });
+router.post('/new_story', (req, res) => {
+  console.log(req.body);
+  res.render('new_story', { title: 'Graphite', stories: 'active'});
+});
+
+router.get('/graphs', (req, res) => {
+  res.render('graphs', { title: 'Graphite', graphs: 'active'});
+});
 
 module.exports = router;
