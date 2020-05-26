c053f41018b360dabeb74f9ad6b533158287b201
Lucas Dale <ld17852@bristol.ac.uk>
2020-05-26 13:33:40 +0100

    random fixed

--- graphite/public/js/new_graph.js
+++ graphite/public/js/new_graph.js
@@ -22,6 +22,7 @@ var display_data = JSON.parse(data);
 if (template.name == "bar") {
   let bar_data = get_bar_data(display_data);
   bar.draw(bar_data);
+  if (options.colour) changeColor(options.colour);
 }
 
 if (template.name == "tree") {
@@ -33,6 +34,7 @@ if (template.name == "tree") {
 if (template.name == "appearance") {
   let hoz_data = get_appearance_data(display_data);
   horizontal_bar.draw(hoz_data);
+  if (options.colour) changeColor(options.colour);
 }
 
 // force_data needs to be outside the if statement as the colour change function relies upon it
@@ -67,50 +69,3 @@ function changeColor(color) {
     force_change_color(color, force_data);
   }
 }
-
-// generates a random int
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-// generates a random colour
-let randomColor = (function () {
-  var golden_ratio_conjugate = 0.618033988749895;
-  var h = Math.random();
-
-  var hslToRgb = function (h, s, l) {
-    var r, g, b;
-
-    if (s == 0) {
-      r = g = b = l; // achromatic
-    } else {
-      function hue2rgb(p, q, t) {
-        if (t < 0) t += 1;
-        if (t > 1) t -= 1;
-        if (t < 1 / 6) return p + (q - p) * 6 * t;
-        if (t < 1 / 2) return q;
-        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-        return p;
-      }
-
-      var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-      var p = 2 * l - q;
-      r = hue2rgb(p, q, h + 1 / 3);
-      g = hue2rgb(p, q, h);
-      b = hue2rgb(p, q, h - 1 / 3);
-    }
-
-    return (
-      "#" +
-      Math.round(r * 255).toString(16) +
-      Math.round(g * 255).toString(16) +
-      Math.round(b * 255).toString(16)
-    );
-  };
-
-  return function () {
-    h += golden_ratio_conjugate;
-    h %= 1;
-    return hslToRgb(h, 0.5, 0.6);
-  };
-})();
